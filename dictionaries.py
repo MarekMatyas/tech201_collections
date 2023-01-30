cd2943f0efee0d15e20392cf8c3f62b968d74dd6
MarekMatyas <matyasamarek@gmail.com>
2023-01-30 15:44:29 -0800

    Dict

---
+++ dictionaries.py
@@ -0,0 +1,49 @@
+# Dictionaries
+# # Dictionaries use key/value pairs
+# # key = a referance to a particular object
+# # value = data storage mechanism you want to use
+#
+# # Create a dictionary
+#
+# student_1 = {
+#     "name": "Susan",
+#     "stream": "DevOps",
+#     "completed_lessons": 4,
+#     "completed_lessons_names": ["variables", "data_types", "set_up"]
+#
+# }
+#
+# # Access data within a dictionary
+# print(student_1["stream"])
+#
+# #changing a value in dictionary
+#
+# student_1["completed_lessons"] = 3
+# print(student_1["completed_lessons"])
+#
+# # Removing items from a dictionary
+#
+# student_1["completed_lessons_names"].remove("data_types")
+# print(student_1["completed_lessons_names"])
+#
+# # Dictionary methods
+# # Keys
+# print(student_1.keys())
+# # getting a certain item from the list without []
+# print(student_1.get("name"))
+#
+# # Get the values of a dictionary
+# print(student_1.values())
+# # outputs array of tuples with key value pairs in dictionary
+# print(student_1.items())
+
+
+dog_list = {
+    "breed": "lab",
+    "name": "Felix",
+    "age": "3",
+    "training_done": ["jump", "crawl", "bark"]
+}
+
+dog_list["training_done"].remove("crawl")
+print(dog_list["training_done"])
---
+++ lists_and_tuples.py
@@ -0,0 +1,58 @@
+# Lists
+#
+# # Lists == Arrays, all about data management
+# # First list
+shopping_list = ["milk", "eggs", "bread", "fruit" , "cheese"]
+# print(type(shopping_list)) #<class 'list'>
+# print(shopping_list)
+#
+# print(shopping_list[0]) # Shows "milk", the first item on the list
+# print(shopping_list[3]) # shows "fruit", the 4th item on the list
+# print(shopping_list[-1]) # shows "cheese", the last item on the list
+#
+# # rewriting a value in our list
+# shopping_list[0] = "sugar"
+# print(shopping_list) # replaces the wanted item on the list ['sugar', 'eggs', 'bread', 'fruit', 'cheese']
+# print(shopping_list[0])
+
+# List methods
+# add to the list
+shopping_list.append("vegetables")
+# print(shopping_list)
+# print(shopping_list[5])
+# print(len(shopping_list))
+
+# Remove from the list
+
+shopping_list.remove("bread")
+# print(len(shopping_list))
+# print(shopping_list)
+
+# Remove list item off the list without specifying it
+#
+# shopping_list.pop()
+# print(shopping_list)
+# shopping_list.pop()
+# print(shopping_list)
+
+# Mixed data type lists
+
+mixture = [1 , 2 , 3.4 , "one" , "two" , "three"]
+# print(mixture)
+#
+# # List slicing
+# print(mixture[1:3]) #[2, 3.4]
+# print(mixture[-2::]) # reverses the order
+#
+# print(mixture[::2]) # bounces over the amount of indexes specified
+
+# Tuples
+
+# Exactly the same as lists, except they are immutable (un-editable)
+# Tuples are useful for elements you want to ensure data stays the same
+
+essential= ("bread", "eggs", "milk")
+print(essential)
+print(essential.count("bread"))
+#essential[0]= "fruit" # will not work. you cant change items in Tuples
+print(essential)
