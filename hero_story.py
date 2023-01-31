a3a5f7dff970923e4872d40571f69c62d2535adc
MarekMatyas <matyasamarek@gmail.com>
2023-01-30 17:00:25 -0800

    Hero_story

---
+++ hero_story.py
@@ -0,0 +1,26 @@
+story_1= {
+    "Beginning": "Once upon a time, there was a small boy without a name living on his own in an old house. He had no parents, friends or family. All of a sudden he noticed bright light shining on the backyard",
+    "Middle": "When he stepped into the light, he felt his body buzzing all the way through. That lasted 5 seconds before it was gone. This 5 seconds changed the boy forever as he became invisible",
+    "End": "The boy with no name became fearless and after spending his whole life inside the house, he then started running through the fields, befriended the wild life and finally was free!"
+}
+print(story_1)
+print(type(story_1))
+print(story_1.keys())
+print(story_1.values())
+print(story_1["Beginning"])
+print(story_1["Middle"])
+print(story_1["End"])
+
+my_super_hero= {
+    "The man in the spaceship": "A man from a planet called GT-49 just discovered planet Earth.",
+    "Abilities": "This man possesses the ability to sense sadness in other beings.",
+    "Help from the man": "The man sensed the boy with no name and decided to deploy the ray of invisibility",
+
+}
+print(my_super_hero)
+print(type(my_super_hero))
+print(my_super_hero.keys())
+print(my_super_hero.values())
+print(my_super_hero["The man in the spaceship"])
+print(my_super_hero["Abilities"])
+print(my_super_hero["Help from the man"])
